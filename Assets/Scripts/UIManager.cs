e2f2c3a45510a6f798d64ca60fcaa73fe58b8930
Lucy Power <74964639+lucypower@users.noreply.github.com>
2024-04-25 11:57:04 +0100

    Upgrades UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,6 +7,12 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] TMP_Text m_moneyText;
 
+    // upgrades
+
+    [SerializeField] GameObject m_upgradeMenu;
+    [SerializeField] GameObject m_playerUpgrades;
+    [SerializeField] GameObject m_employeeUpgrades;
+
     PlayerStatistics m_playerStatistics;
 
     private void Start()
@@ -17,5 +23,35 @@ public class UIManager : MonoBehaviour
     private void Update()
     {
         m_moneyText.text = "Money : " + m_playerStatistics.m_money;
+    }
+
+    public void UpgradeMenu(bool showMenu)
+    {
+        switch (showMenu)
+        {
+            case true:
+
+                m_upgradeMenu.SetActive(true);
+
+            break;
+
+            case false:
+
+                m_upgradeMenu.SetActive(false);
+
+            break;
+        }
+    }
+
+    public void PlayerUpgrades()
+    {
+        m_playerUpgrades.SetActive(true);
+        m_employeeUpgrades.SetActive(false);
+    }
+
+    public void EmployeeUpgrades()
+    {
+        m_employeeUpgrades.SetActive(true);
+        m_playerUpgrades.SetActive(false);
     }
 }
