12b32bb0bddfaf00f35e832fb705a190da06fd50
lucypower <luciellepenguin9@gmail.com>
2024-05-09 00:09:41 +0100

    basic iap (kinda sh*t rn)

---
+++ Assets/Scripts/Monetisation/InitializeGameServices.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Services.Core;
+using Unity.Services.Core.Environments;
+using UnityEngine;
+
+public class InitializeGameServices : MonoBehaviour
+{
+    async void Start()
+    {
+        try
+        {
+            var options = new InitializationOptions()
+
+            #if UNITY_EDITOR || DEVELOPMENT_BUILD
+
+                .SetEnvironmentName("test");
+
+            #else
+
+                .SetEnvironmentName("production");
+
+            #endif
+
+            await UnityServices.InitializeAsync(options);
+        }
+        catch (Exception exception)
+        {
+            Debug.Log("Unity game services failed to initialize :( " + exception);
+        }
+    }
+}
